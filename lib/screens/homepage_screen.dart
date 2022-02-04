0b8762f975099507e136ab36133478aaf901dd4b
sumitvarun <sumit.varun@rocketmail.com>
2022-02-04 17:29:10 +0530

    Update

--- lib/screens/homepage_screen.dart
+++ lib/screens/homepage_screen.dart
@@ -19,20 +19,81 @@ class _HomePageState extends State<HomePage> {
           headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
             return <Widget>[
               CupertinoSliverNavigationBar(
+                backgroundColor: Colors.transparent,
                 largeTitle: Text(
                   'COVIDTRACBOOK',
                   style: GoogleFonts.exo2(
                       color: Colors.black, fontWeight: FontWeight.normal),
                 ),
-                middle: Text(
-                  ' A Covid19 Tracker Application',
-                  style: GoogleFonts.exo2(color: Colors.black),
-                ),
+                //middle: Text(
+                // ' A Covid19 Tracker Application',
+                //style: GoogleFonts.exo2(color: Colors.black),
+                // ),
               )
             ];
           },
-          body: Center(
-            child: Column(),
+          body: SafeArea(
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.all(5.0),
+                          height: 100.0,
+                          decoration: const BoxDecoration(color: Colors.red),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.all(5.0),
+                          height: 100.0,
+                          decoration: const BoxDecoration(color: Colors.blue),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.all(5.0),
+                          height: 100.0,
+                          decoration: const BoxDecoration(color: Colors.green),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.all(5.0),
+                          height: 100.0,
+                          decoration:
+                              const BoxDecoration(color: Colors.deepOrange),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.all(5.0),
+                          height: 100.0,
+                          decoration: const BoxDecoration(color: Colors.yellow),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.all(5.0),
+                          height: 100.0,
+                          decoration: const BoxDecoration(color: Colors.indigo),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
           ),
         ),
       ),
