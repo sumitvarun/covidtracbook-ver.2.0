aa95bf3e1874d0dd612af52615ab6cb11e91c6ae
sumitvarun <sumit.varun@rocketmail.com>
2022-02-11 23:38:25 +0530

    Update

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -41,7 +41,7 @@ class _SplashScreenState extends State<SplashScreen> {
           decoration: const BoxDecoration(
             image: DecorationImage(
               image: AssetImage(
-                  'lib/assets/image/CovidTracBook_2_0_copy_2_logopng__1__AdobeCreativeCloudExpress.gif'),
+                  'lib/assets/image/CovidTracBook_copy3545_AdobeCreativeCloudExpress.gif'),
               fit: BoxFit.contain,
             ),
           ),
